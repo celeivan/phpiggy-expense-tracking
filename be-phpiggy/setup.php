762fbd6995a07dc8574a407cc650d0de458603a0
Njabulo Ivan Cele <developer@ncitsolutions.co.za>
2024-02-11 12:28:53 +0200

    wip - Initial setup

---
+++ be-phpiggy/setup.php
@@ -0,0 +1,3 @@
+<?php
+
+echo "Setup completed successfully.";
---
+++ be-phpiggy/src/App/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+// Load and conf the app/framework
+
+declare(strict_types=1);
+
+require __DIR__."/../../vendor/autoload.php";
+
+use Framework\App;
+
+$app = new App();
+
+return $app;
