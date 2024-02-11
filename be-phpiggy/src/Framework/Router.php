b717018e7baa09e374339303f50d25a02e2c26cc
Njabulo Ivan Cele <developer@ncitsolutions.co.za>
2024-02-11 14:29:27 +0200

    Controller added

--- be-phpiggy/src/Framework/Router.php
+++ be-phpiggy/src/Framework/Router.php
@@ -3,22 +3,41 @@ declare(strict_types=1);
 
 namespace Framework;
 
-class Router{
+class Router
+{
     private array $routes = [];
-    
-    public function add(string $method, string $path){
+
+    public function add(string $method, string $path, array $controller)
+    {
         $this->routes[] = [
             'path' => $this->normalizePath($path),
-            'method' => strtoupper($method)
+            'method' => strtoupper($method),
+            'controller' => $controller
         ];
     }
 
-    private function normalizePath(string $path):string
+    private function normalizePath(string $path): string
     {
         $path = trim($path, '/');
-        $path ="/{$path}/";
-        $path = preg_replace('#[/]{2,}#','/',$path);
-    
+        $path = "/{$path}/";
+        $path = preg_replace('#[/]{2,}#', '/', $path);
+
         return $path;
+    }
+
+    public function dispatch(string $path, string $method)
+    {
+        $path = $this->normalizePath($path);
+        $method = strtoupper($method);
+
+        foreach ($this->routes as $route) {
+            if (!preg_match("#^{$route['path']}$#", $path) || $route['method'] !== $method)
+                continue;
+
+            [$class, $function] = $route['controller'];
+
+            $controllerInstance = new $class;
+            $controllerInstance->{$function}();
+        }
     }
 }
